7134af042f19c59c7898c5cd06a16bf20eb89b44
Object <87041394+ObjectRetros@users.noreply.github.com>
2023-07-08 15:05:21 +0200

    Add files via upload

--- nitro-index-fix.sh
+++ nitro-index-fix.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-file_path="/home/dennis/dev/habgencms/public/client/nitro/nitro-react/dist/index.html"
+file_path="C:/inetpub/wwwroot/atomcms/public/client/nitro/nitro-react/dist/index.html"
 
-# Read the content of the file
-content=$(<"$file_path")
+awk '{
+  gsub("/favicon.ico", "./favicon.ico");
+  gsub("/apple-touch-icon.png", "./apple-touch-icon.png");
+  gsub("/favicon-32x32.png", "./favicon-32x32.png");
+  gsub("/favicon-16x16.png", "./favicon-16x16.png");
+  gsub("/site.webmanifest", "./site.webmanifest");
+  gsub("/safari-pinned-tab.svg", "./safari-pinned-tab.svg");
+  gsub("/assets/index-[^.]*\\.js", "./assets/index.js");
+  gsub("/assets/vendor-[^.]*\\.js", "./assets/vendor.js");
+  gsub("/assets/nitro-renderer-[^.]*\\.js", "./assets/nitro-renderer.js");
+  gsub("/src/assets/index.css", "./src/assets/index.css");
+  gsub("/renderer-config.json", "./renderer-config.json");
+  gsub("/ui-config.json", "./ui-config.json");
+  print
+}' "$file_path" > "$file_path.tmp"
 
-# Replace the paths in the content
-content="${content//\/favicon.ico/\.\/favicon.ico}"
-content="${content//\/apple-touch-icon.png/\.\/apple-touch-icon.png}"
-content="${content//\/favicon-32x32.png/\.\/favicon-32x32.png}"
-content="${content//\/favicon-16x16.png/\.\/favicon-16x16.png}"
-content="${content//\/site.webmanifest/\.\/site.webmanifest}"
-content="${content//\/safari-pinned-tab.svg/\.\/safari-pinned-tab.svg}"
-content="${content//\/assets\/index-f42fc6a0.js/\.\/assets\/index-f42fc6a0.js}"
-content="${content//\/assets\/vendor-56d266b8.js/\.\/assets\/vendor-56d266b8.js}"
-content="${content//\/assets\/nitro-renderer-0af3579c.js/\.\/assets\/nitro-renderer-0af3579c.js}"
-content="${content//\/src\/assets\/index.css/\.\/src\/assets\/index.css}"
-content="${content//\/renderer-config.json/\.\/renderer-config.json}"
-content="${content//\/ui-config.json/\.\/ui-config.json}"
-
-# Write the modified content back to the file
-echo "$content" > "$file_path"
+# Overwrite the original file with the modified content
+mv "$file_path.tmp" "$file_path"
 
 echo "File updated successfully."
